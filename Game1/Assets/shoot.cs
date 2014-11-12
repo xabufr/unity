2885f58223705e78de243965301afcfd0cdce91a
Thomas Loubiou <xabufr@gmail.com>
2014-11-12 19:20:29 +0100

    initial commit

---
+++ Game1/Assets/shoot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour
+{
+
+		public Transform origin;
+		public Object bullet;
+		public GameObject fire;
+		private float lastShoot = 0;
+
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetButton ("Fire1") && Time.realtimeSinceStartup - lastShoot > (1.0/4.0)) {
+						lastShoot = Time.realtimeSinceStartup;
+						GameObject newBullet = (GameObject)Instantiate (bullet, origin.position, origin.rotation);
+						Destroy (newBullet, 1);
+						newBullet.rigidbody.AddRelativeForce (Vector3.forward * 2000);
+						foreach (MeshRenderer o in fire.GetComponentsInChildren<MeshRenderer>()) {
+								o.enabled = true;
+						}
+				} else {
+						
+						foreach (MeshRenderer o in fire.GetComponentsInChildren<MeshRenderer>()) {
+								o.enabled = false;
+						}
+				}
+		}
+}
